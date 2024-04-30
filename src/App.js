e719c64a0199a2a77cc1093a87c4c1084253cb1d
kiryu <13matheusilva@gmail.com>
2024-04-30 13:00:26 -0300

    section hero component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/header/header';
+import SectionHero from './components/hero/hero';
 import './App.css';
 
 function App() {
   return (
     <main className="container">
       <Header />
+
+      <SectionHero />
     </main>
   );
 }
---
+++ src/components/hero/hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import illustrationWorking from '../../images/illustration-working.svg'
+
+const SectionHero = () => {
+    return (
+        <section className="hero">
+            <div className="info">
+                <h1 className="title">More than just shorter links</h1>
+                <p className="description">Build your brand’s recognition and get detailed insights
+                    on how your links are performing.</p>
+                <a href="/">Get Started</a>
+            </div>
+            <img src={illustrationWorking} alt="illustration working"></img>
+        </section>
+    )
+}
+
+export default SectionHero
