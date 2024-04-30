73c3dc7e8855c458d6f824b5109320ba733f15a8
kiryu <13matheusilva@gmail.com>
2024-04-30 14:31:53 -0300

    footer component add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/header/header';
 import SectionHero from './components/hero/hero';
 import SectionShortening from './components/section-shortening/section-shortening';
 import SectionAdvanced from './components/section-advanced/section-advanced';
+import SectionBoost from './components/section-boost/section-boost';
+import Footer from './components/footer/footer';
 import './App.css';
 
 function App() {
@@ -14,6 +16,10 @@ function App() {
       <SectionShortening />
 
       <SectionAdvanced />
+
+      <SectionBoost />
+
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import logoLight from "../../images/logo-light.svg"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <a href="/">
+                    <img src={logoLight} alt="logo"></img>
+                </a>
+
+                <div className="links">
+                    <h2 className="title">Features</h2>
+                    <ul>
+                        <li><a href="/">Link Shortening</a></li>
+                        <li><a href="/">Branded Links</a></li>
+                        <li><a href="/">Analytics</a></li>
+                    </ul>
+                </div>
+                
+                <div className="links">
+                    <h2 className="title">Resources</h2>
+                    <ul>
+                        <li><a href="/">Blog</a></li>
+                        <li><a href="/">Developers</a></li>
+                        <li><a href="/">Support</a></li>
+                    </ul>
+                </div>
+
+                <div className="links">
+                    <h2 className="title">Company</h2>
+                    <ul>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Our Team</a></li>
+                        <li><a href="/">Careers</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div>
+
+                <ul className="social">
+                    <li><a href="/"><i className="fa-brands fa-square-facebook"></i></a></li>
+                    <li><a href="/"><i className="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="/"><i className="fa-brands fa-pinterest"></i></a></li>
+                    <li><a href="/"><i className="fa-brands fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
