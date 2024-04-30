abc2e8fd57af43d1afdc44d0cd3edc01b280136f
kiryu <13matheusilva@gmail.com>
2024-04-30 13:58:54 -0300

    section boost component add

---
+++ src/components/section-boost/section-boost.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SectionBoost = () => {
+    return(
+        <section className="boost">
+            <h2 className="title">Boost your links today</h2>
+            <a href="/">Get Started</a>
+        </section>
+    )
+}
+
+export default SectionBoost
