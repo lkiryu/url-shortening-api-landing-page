a34652318c4ccd78cb515098216ccde06f1da460
kiryu <13matheusilva@gmail.com>
2024-05-02 14:48:41 -0300

    header style add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,24 +4,26 @@ import logo from "../../images/logo.svg"
 const Header = () => {
     return (
         <header className="header">
-            <a href="/">
-                <img src={logo} alt="logo"></img>
-            </a>
+            <div className="header-content">
+                <a href="/">
+                    <img src={logo} alt="logo"></img>
+                </a>
 
-            <nav>
-                <div className="nav-lists">
-                    <ul className="nav-list">
-                        <li className="nav-link"><a href="/">Features</a></li>
-                        <li className="nav-link"><a href="/">Pricing</a></li>
-                        <li className="nav-link"><a href="/">Resources</a></li>
-                    </ul>
+                <nav>
+                    <div className="nav-lists">
+                        <ul className="nav-list">
+                            <li className="nav-link"><a href="/">Features</a></li>
+                            <li className="nav-link"><a href="/">Pricing</a></li>
+                            <li className="nav-link"><a href="/">Resources</a></li>
+                        </ul>
 
-                    <ul className="nav-list">
-                        <li className="nav-link"><a href="/">Login</a></li>
-                        <li className="nav-link sign-up"><a href="/">Sign Up</a></li>
-                    </ul>
-                </div>
-            </nav>
+                        <ul className="nav-list">
+                            <li className="nav-link"><a href="/">Login</a></li>
+                            <li className="nav-link sign-up"><a href="/">Sign Up</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 }
