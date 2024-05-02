b441eebf5d5199107a5b1df0cf2f9c8922047ba6
kiryu <13matheusilva@gmail.com>
2024-05-02 15:58:52 -0300

    section shortening style add

--- src/components/section-shortening/section-shortening.js
+++ src/components/section-shortening/section-shortening.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import './section-shortening.css'
+
 const SectionShortening = () => {
     return (
         <section className="shortening">
             <form className="shortening-inputs">
                 <div className="link-input">
-                    <input type="url" name="url" id="url" placeholder="Shorten a link here..."></input>
-                    <p className="input-error">Please add a link</p>
+                    <input type="url" name="url" id="url" className="url" placeholder="Shorten a link here..."></input>
+                    <p className="input-error none">Please add a link</p>
                 </div>
-                <input type="submit" value="Shorten It!"></input>
+                <input type="submit" className="submit" value="Shorten It!"></input>
             </form>
         </section>
     )
